44c444b3e9d71d3a25483200b716662daee186ea
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-12-06 16:14:26 +0100

    port to 1.20.3

--- src/main/java/io/github/fourmisain/dyeallthethings/DyeAllTheThings.java
+++ src/main/java/io/github/fourmisain/dyeallthethings/DyeAllTheThings.java
@@ -1,10 +1,5 @@
 package io.github.fourmisain.dyeallthethings;
 
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
-import net.fabricmc.loader.api.metadata.version.VersionPredicate;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.HorseArmorItem;
@@ -12,7 +7,6 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class DyeAllTheThings {
@@ -22,27 +16,9 @@ public class DyeAllTheThings {
 			.collect(Collectors.toList());
 	}
 
-	public static boolean testVersion(String modId, String versionRange) {
-		try {
-			Optional<ModContainer> container = FabricLoader.getInstance().getModContainer(modId);
-			if (!container.isPresent())
-				return false;
-
-			VersionPredicate pred = VersionPredicate.parse(versionRange);
-			Version version = container.get().getMetadata().getVersion();
-
-			return pred.test(version);
-		} catch (VersionParsingException e) {
-			throw new AssertionError(e);
-		}
-	}
-
 	public static void lateInit() {
-		// CauldronBehavior didn't exist in 1.16 (and the behavior worked for all DyeableItem by default)
-		if (!testVersion("minecraft", "~1.16.0")) {
-			for (Item item : getArmorItems()) {
-				CauldronBehavior.WATER_CAULDRON_BEHAVIOR.putIfAbsent(item, CauldronBehavior.CLEAN_DYEABLE_ITEM);
-			}
+		for (Item item : getArmorItems()) {
+			CauldronBehavior.WATER_CAULDRON_BEHAVIOR.map().putIfAbsent(item, CauldronBehavior.CLEAN_DYEABLE_ITEM);
 		}
 	}
 }
