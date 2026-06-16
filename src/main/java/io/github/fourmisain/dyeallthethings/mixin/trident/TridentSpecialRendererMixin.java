599d8c0a02bc9de9fefae1158444b28e1b5ebd86
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-06-16 19:22:29 +0200

    port to 26.2

--- src/main/java/io/github/fourmisain/dyeallthethings/mixin/trident/TridentSpecialRendererMixin.java
+++ src/main/java/io/github/fourmisain/dyeallthethings/mixin/trident/TridentSpecialRendererMixin.java
@@ -1,13 +1,23 @@
 package io.github.fourmisain.dyeallthethings.mixin.trident;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.vertex.PoseStack;
+import io.github.fourmisain.dyeallthethings.DyeAllTheThings;
 import io.github.fourmisain.dyeallthethings.render.Dyeable;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.renderer.OrderedSubmitNodeCollector;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.special.TridentSpecialRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.Identifier;
+import net.minecraft.util.Unit;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import static io.github.fourmisain.dyeallthethings.DyeAllTheThings.WHITE;
+import static net.minecraft.client.renderer.entity.ThrownTridentRenderer.TRIDENT_LOCATION;
 
 @Mixin(TridentSpecialRenderer.class)
 public abstract class TridentSpecialRendererMixin implements Dyeable {
@@ -24,12 +34,20 @@ public abstract class TridentSpecialRendererMixin implements Dyeable {
 		return dyeallthethings$color;
 	}
 
-	@ModifyArg(
+	@SuppressWarnings("unchecked")
+	@WrapOperation(
 		method = "submit",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/SubmitNodeCollector;submitModelPart(Lnet/minecraft/client/model/geom/ModelPart;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/rendertype/RenderType;IILnet/minecraft/client/renderer/texture/TextureAtlasSprite;ZZILnet/minecraft/client/renderer/feature/ModelFeatureRenderer$CrumblingOverlay;I)V"),
-		index = 8
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/renderer/OrderedSubmitNodeCollector;submitModel(Lnet/minecraft/client/model/Model;Ljava/lang/Object;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/resources/Identifier;IIILnet/minecraft/client/renderer/feature/ModelFeatureRenderer$CrumblingOverlay;)V",
+			ordinal = 0
+		)
 	)
-	public int dye(int tintedColor) {
-		return dyeallthethings$color;
+	private <S> void submitColoredModel(OrderedSubmitNodeCollector instance, Model<? extends S> model, S state, PoseStack poseStack, Identifier texture, int lightCoords, int overlayCoords, int outlineColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay, Operation<Void> original) {
+		if (dyeallthethings$color != DyeAllTheThings.WHITE) {
+			instance.submitModel((Model<Unit>) model, Unit.INSTANCE, poseStack, model.renderType(TRIDENT_LOCATION), lightCoords, OverlayTexture.NO_OVERLAY, dyeallthethings$color, null, outlineColor, crumblingOverlay);
+		} else {
+			original.call(instance, model, state, poseStack, texture, lightCoords, overlayCoords, outlineColor, crumblingOverlay);
+		}
 	}
 }
